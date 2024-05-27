7b6a11ed8f32ba6bee5662e3061341c47ce36236
khurshid idrees <umarkhurshid3@gmail.com>
2024-05-27 21:12:17 +0530

    update in navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
-   <>
-   
-   </>
+    <>
+      <NavBar />
+    </>
   );
 }
 
