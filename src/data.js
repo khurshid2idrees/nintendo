cb1f9277774bafa986f0d4d6f049b986b4b8e1ca
khurshid idrees <umarkhurshid3@gmail.com>
2024-05-29 20:26:31 +0530

    udpate in data and navbar

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const NavbarData = [
+  {
+    categoriess: [
+      {
+        id: 1,
+        name: "All Categories",
+      },
+      {
+        id: 2,
+        name: "Games",
+      },
+      {
+        id: 3,
+        name: "Hardware",
+      },
+      {
+        id: 4,
+        name: "Merchandise",
+      },
+      {
+        id: 5,
+        name: "News & Events",
+      },
+      {
+        id: 6,
+        name: "Support",
+      },
+    ],
+  },
+];
